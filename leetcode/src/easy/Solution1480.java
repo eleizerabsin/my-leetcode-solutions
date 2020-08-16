ebc52b93d2cbe45358885600efd08c449697c888
eabsin <eleizer.absin@gmail.com>
2020-08-16 16:49:18 +0800

    Added solution for #1480

---
+++ leetcode/src/easy/Solution1480.java
@@ -0,0 +1,21 @@
+package easy;
+
+/**
+ * 1480. Running Sum of 1d Array
+ * https://leetcode.com/problems/running-sum-of-1d-array/
+ * @author user
+ *
+ */
+public class Solution1480 {
+    public int[] runningSum(int[] nums) {
+        if(nums.length == 1) {
+            return nums;
+        }
+        
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i-1];
+        }
+        
+        return nums;
+    }
+}
